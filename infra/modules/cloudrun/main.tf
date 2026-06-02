9615e0c46fedafc948a1b9f3ec7f958c032c7e79
Yashwanth Keerthi <iamkeerthiyashwanth@gmail.com>
2026-06-02 21:59:21 +0100

    ci: remove public IAM binding from terraform - managed separately

--- infra/modules/cloudrun/main.tf
+++ infra/modules/cloudrun/main.tf
@@ -39,11 +39,3 @@ resource "google_cloud_run_v2_service" "ingestion_api" {
     project     = "pestpulse"
   }
 }
-
-resource "google_cloud_run_v2_service_iam_member" "public" {
-  project  = var.project_id
-  location = var.region
-  name     = google_cloud_run_v2_service.ingestion_api.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
